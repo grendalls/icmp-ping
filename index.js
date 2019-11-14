c6833539a8fa888aa8a184153bcf89aacab1f3d7
Dmitriy Denisov <aldzir3@gmail.com>
2019-11-15 01:28:47 +0300

    Make a build client

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
 const cors = require("cors");
+const path = require("path");
 const morgan = require("morgan");
 const sequelizeStore = require("express-session-sequelize")(session.Store);
 
 const app = expess();
 
 app.use(morgan("combined"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const userRouter = require("./routes/user");
 const deviceRouter = require("./routes/device");
